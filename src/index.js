e700b25d3c888b4226c40f9326fbba23a903ee3b
rmzvr <rmzvr@proton.me>
2023-01-31 14:30:51 +0100

    hw4 update

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<Provider store={store}>
-		<BrowserRouter basename='/courses-app'>
+		<HashRouter basename='/courses-app'>
 			<App />
-		</BrowserRouter>
+		</HashRouter>
 	</Provider>
 );
 
